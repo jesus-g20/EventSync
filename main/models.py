743153f4b3bec585756a4477a7f75d1ff1dd8b15
insomniac157 <143749297+insomniac157@users.noreply.github.com>
2024-12-06 23:10:38 -0500

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+import psycopg2
+
+DB_CONFIG = {
+    "dbname": "eventsync",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": "5432",
+}
+
+def get_db_connection():
+    """Establish and return a connection to the database."""
+    return psycopg2.connect(**DB_CONFIG)
